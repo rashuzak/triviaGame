4df24e341155d92d17f50731c8fc570f5195a4ca
rashu <rashuzak2@gmail.com>
2020-02-29 16:24:21 -0600

    still need to work a few things outlike the audio and needs a few changes

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# triviaGame
+
 homework #5
+TRIVIA GAME:    THE OSCARS
+
+Table of Contents
+
+Overview
+
+Technologies/Functions
+
+
+
+OverView  Game rules:
+
+.When you click Start, you will be given a question with four multiple-choice options. Total of 10 questions.
+
+.You have 30 seconds to answer each of the question before time runs out.
+
+.When the game ends, your corret and incorrect score will be displayed for you.
+
+
+
+
+Technologies/Functions:
+
+setInterv()
+
+clearInterval()
+
+Google Fonts
+
+This project was built with JavaScript, jQuery, HTML, and CSS. Most all of the HTML content is generated dynamically through JavaScript.
+
+
+
+
+Game notes:
+
+The trivia question  are from https://www.funtrivia.com/en/Movies/Oscars--General-Trivia-18808_14.html
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		time : 30,
 		reset: function() {
 			this.time = 30;
-			$(".timer").html("<h3>" + this.time + " seconds remaining</h3>");
+			$('.timer').html('<h3>' + this.time + ' seconds remaining</h3>');
 		},
 		start: function() {
 			counter = setInterval(countdownTimer.count, 1000);	
@@ -14,14 +14,13 @@ $(document).ready(function() {
 		},
 		count: function() {
 				countdownTimer.time--;
-				console.log(countdownTimer.time);
-//				$('.timer').html(countdownTimer.time);
+
 			if (countdownTimer.time >= 0) {
-				$(".timer").html("<h3>" + countdownTimer.time + " seconds remaining</h3>");
+				$('.timer').html('<h3>' + countdownTimer.time + ' seconds remaining</h3>');
 			}
 			else {
 				index++;
-				answerWrong();
+				answerCorrect();
 				countdownTimer.reset();
 				if (index < questionArray.length) {
 					loadQuestion(index);
@@ -33,128 +32,126 @@ $(document).ready(function() {
 		}
 	};
 
-	//got trivia questions from https://www.funtrivia.com/en/Movies/Oscars--General-Trivia-18808_14.html
-
 var correct = 0;
-var wrong = 0;
-var q1 = {
-	question : "Who won th Oscars for Best Actor for playing Hannibal Lecter?",
-	possibleAnswers : ["A. Jack Nicholson",
+var incorrect = 0;
+
+//created an array of oscar trivia questions
+
+var  questionArray = [{
+      
+	question : "Who won the Oscars for Best Actor for playing Hannibal Lecter?",
+	answerChoices : ["A. Jack Nicholson",
 				 "B. Peter Finch",
 				 "C. Anthony Hopkins",
 				 "D. Ben Kingsley"],
 	flags : [false, false, true, false],
-	answer : "C. Anthony Hopkins"
-};
+  answer : "C. Anthony Hopkins"
+    },
 
-var q2 = {
+
+ {
 	question: "Julie Andrews picked up the Oscar for Best Actress after her first film performance. Which film did she win the award for?",
-	possibleAnswers: ["A. Sound of Music",
+	answerChoices: ["A. Sound of Music",
 				 "B. Mary Poppins",
 				 "C. Victor/Victoria",
 				 "D. Peter Pan"],
 	flags : [false, true, false, false],
 	answer : "B. Mary Poppins"
-};
+},
 
-var q3 = {
+{
 	question : "Which of these actors won the Best Actor award consecutively for two years?",
-	possibleAnswers : ["A. Roberto Benigni",
+	answerChoices : ["A. Roberto Benigni",
 				 "B. Geoffrey Rushr",
 				 "C. Jeremy Irons",
 				 "D. Tom Hanks"],
 	flags : [false, false, false, true],
 	answer : "D. Tom Hanks"
-};
+},
 
-var q4 = {
+ {
 	question : "Which woman has written a book, that has become a film, that has earned Oscar nominations?",
-	possibleAnswers : ["A. Alice Walker",
+	answerChoices : ["A. Alice Walker",
 				 "B. Ann Tyler",
 				 "C. Anne Rice",
 				 "D. All Of Them"],
 	flags : [false, false, false, true],
 	answer : "D. All Of Them"
-};
+},
 
-var q5 = {
+ {
 	question : "What was one of the Oscars that 'The Godfather' won?",
-	possibleAnswers : ["A. Best Director" ,
+	answerChoices : ["A. Best Director" ,
 				 "B. Best Picture",
 				 "C. Best Screenplay",
 				 "D. Best Actress"],
 	flags : [false, true, false, false],
 	answer : "B. Best Picture"
-};
+},
 
-var q6 = {
+ {
 	question : "The movie 'Titanic', having earned 11 oscars, including Best Picture, and grossing over $600 million in the U.S., is probably the most successful film ever. Which of the following Best Picture Oscar winners has earned the most money at the box office?",
-	possibleAnswers : ["A. Gone With The Wind",
+	answerChoices : ["A. Gone With The Wind",
 				 "B. The Sound Of Music",
 				 "C. The Silence Of The Lambs",
 				 "D. Forrest Gump"],
 	flags : [false, false, false, true],
 	answer : "D. Forrest Gump"
-};
+},
 
-var q7 = {
+ {
 	question : "Which of the following movies won the Academy Award for Best Picture?",
-	possibleAnswers : ["A. Jaws (1975)",
+	answerChoices : ["A. Jaws (1975)",
 				 "B. Taxi Driver (1976)",
 				 "C. The Deer Hunter (1978)",
 				 "D. Star Wars (1978)"],
 	flags : [false, false, true, false],
 	answer : "C. The Deer Hunter (1978)"
-};
+},
 
-var q8 = {
+ {
 	question : "Which of the following films won the most Academy Awards without winning Best Picture?",
-	possibleAnswers : ["A. A Place In The Sun",
+	answerChoices : ["A. A Place In The Sun",
 				 "B. Cabaret",
 				 "C.Star Wars",
 				 "D.The King And I"],
 	flags : [false, true, false, false],
 	answer : "B. Cabaret"
-};
+},
 
-var q9 = {
+ {
 	question : "Of these four box office blockbusters, which one won the most Oscars?",
-	possibleAnswers : ["A. E.T.",
+	answerChoices : ["A. E.T.",
 				 "B. The Matrix",
 				 "C. Jurassic Park",
 				 "D. Star Wars"],
 	flags : [false, false, false, true],
 	answer : "D. Star Wars"
-};
+},
 
-var q10 = {
+ {
 	question : "Which of the following films was the first one to win an Oscar for Best Make-up?",
-	possibleAnswers : ["A. The Exorcist",
+	answerChoices : ["A. The Exorcist",
 				  "B. An American Werewolf in London",
 				  "C. Quest For Fire",
 				  "D. Ms. Doubtfire"],
 	flags : [false, true, false, false],
 	answer : "B. An American Werewolf in London"
 }
-
-var questionArray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
+    ];
 
 function loadQuestion(questionSelection) {
 	console.log(questionSelection);
 	countdownTimer.reset();
   $(".question").html("<h3>" + questionArray[questionSelection].question + "</h3>");
-  $("#buttonA").text(questionArray[questionSelection].possibleAnswers[0]).show();
-  $("#buttonB").text(questionArray[questionSelection].possibleAnswers[1]).show();
-  $("#buttonC").text(questionArray[questionSelection].possibleAnswers[2]).show();
-  $("#buttonD").text(questionArray[questionSelection].possibleAnswers[3]).show();
-//  getAnswer();  
-//  nextQuestion(index);
+  $("#buttonA").text(questionArray[questionSelection].answerChoices[0]).show();
+  $("#buttonB").text(questionArray[questionSelection].answerChoices[1]).show();
+  $("#buttonC").text(questionArray[questionSelection].answerChoices[2]).show();
+  $("#buttonD").text(questionArray[questionSelection].answerChoices[3]).show();
+
 }
 
-//function nextQuestion() {
-//	index = index++;
-//	console.log(index);
-//}
+
 
 function setup() {
 	index = 0;
@@ -188,43 +185,21 @@ function answerCorrect() {
 	console.log("correct");
 }
 
-function answerWrong() {
-	wrong++;
+function answerIncorrect() {
+	incorrect++;
 	alert("Incorrect!");
-	console.log("wrong");
+	console.log("incorrect");
 }
 
 function showScore() {
 	$('.question').empty();
 	$('.question').append("<h2><p>" + correct + " correct</p></h2>");
-	$('.question').append("<h2><p>" + wrong + " incorrect</p></h2>");
+	$('.question').append("<h2><p>" + incorrect + " incorrect</p></h2>");
 	countdownTimer.stop();
 	$('.timer').empty();
 
 }
-//		for (var i=0; i<questionArray.length; i++) {
-//			$('.question').append('<p>'+questionArray[i].question+'</p>');
-//			for (var j=0; j<questionArray[i].possibleAnswers.length; j++) {
-//				$('.answers').append('<span><button id="possibleAnswer">' + questionArray[i].possibleAnswers[j]+ '</button></span>');
-//			}
-//			$('#possibleAnswers').on('click', function() {
 
-
-//		console.log("click");
-//		countdownTimer.start();
-//		for (var i = 0; i < questionArray.length; i++) {
-//			console.log(i);
-
-//			$('.timer').html('<h3>'+countdownTimer.time + ' seconds remaining</h3>');
-//			$('.question').html(questionArray[i].question);
-//			while (countdownTimer != 0) {
-
-//			}
-		
-//	});
-//	$('#startButton').click(countdownTimer.start);
-
-//}
 setup();
 $('.answerchoice').on('click', function() {
  console.log($(this));
@@ -240,22 +215,22 @@ $('.answerchoice').on('click', function() {
  if ((answerChosen == 'A') && (questionArray[index].flags[0] == true)) {
  	answerCorrect();
  } else if (answerChosen == 'A') {
- 	answerWrong();
+ 	answerIncorrect();
  }
  if ((answerChosen == 'B') && (questionArray[index].flags[1] == true)) {
  	answerCorrect();
  } else if (answerChosen == 'B') {
- 	answerWrong();
+ 	answerIncorrect();
  }
 if ((answerChosen == 'C') && (questionArray[index].flags[2] == true)) {
  	answerCorrect();
  } else if (answerChosen == 'C') {
- 	answerWrong();
+ 	answerIncorrect();
  }
 if ((answerChosen == 'D') && (questionArray[index].flags[3] == true)) {
  	answerCorrect();
  } else if (answerChosen == 'D') {
- 	answerWrong();
+ 	answerIncorrect();
  }
 
  $(".question").text('');
@@ -272,6 +247,4 @@ if ((answerChosen == 'D') && (questionArray[index].flags[3] == true)) {
  }
 });
 
-
-//	$('#start').click(countdownTimer.start);
 });
